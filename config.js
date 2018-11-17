3756324fec4a7b294514bb8760b6841cc1a42108
BAMHiggin <bamhiggin@gmail.com>
2018-11-17 00:33:00 -0600

    added styling, removed gitignore config so there are no complications when deployed

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCnFAEhHAdZoun1fxv9xNfEKeCJBBqArkU",
+    authDomain: "traintimes-bda19.firebaseapp.com",
+    databaseURL: "https://traintimes-bda19.firebaseio.com",
+    projectId: "traintimes-bda19",
+    storageBucket: "traintimes-bda19.appspot.com",
+    messagingSenderId: "218571669614"
+};
